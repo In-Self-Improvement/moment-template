c6da2d5636f3f7bf6cec6c3cdf45b77839bf7948
Bong <borobong230@gmail.com>
2023-06-22 23:25:54 +0900

    feat: Add why did you render lib #20

--- App.tsx
+++ App.tsx
@@ -3,7 +3,12 @@ import {StyleSheet, View} from 'react-native';
 import MomentScreen from '~/index';
 
 interface Props {}
-
+if (process.env.NODE_ENV === 'development') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  whyDidYouRender(React, {
+    trackAllPureComponents: true,
+  });
+}
 const App = ({}: Props) => {
   const styles = createStyles();
 
